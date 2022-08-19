cb529ff5718ec667de76dd2d9373fd079b554ff7
modehans <monicaglezsanch@gmail.com>
2022-08-19 10:14:59 +0200

    Add call to API and render prhases list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 // Fichero src/components/App.js
 import '../styles/App.scss';
 import '../styles/core/reset.scss';
-
+import { useEffect, useState } from 'react';
+import callToApi from '../services/api';
 function App() {
+  const [dataPhrases, setDataPhrases] = useState([]);
+  useEffect(() => {
+    callToApi().then((response) => {
+      setDataPhrases(response);
+    });
+  }, []);
+
+  const renderPhrases = () => {
+    return dataPhrases.map((phrase, index) => (
+      <li key={index} className="list__phrase">
+        <p>
+          {phrase.quote}{' '}
+          <span className="list__phrase-character"> - {phrase.character}</span>
+        </p>
+      </li>
+    ));
+  };
+
   return (
     <div className="container">
-      <h1 className="title">Frases de Friends</h1>
+      <header>
+        <h1 className="title">Frases de Friends</h1>
+      </header>
+      <main>
+        <ul className="list">{renderPhrases()}</ul>
+      </main>
     </div>
   );
 }
