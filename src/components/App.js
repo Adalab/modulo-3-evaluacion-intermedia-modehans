bfd8822fd388269d8be744d7241fb4b95b2997d6
modehans <monicaglezsanch@gmail.com>
2022-08-19 13:55:20 +0200

    Create bonus 3

--- src/components/App.js
+++ src/components/App.js
@@ -3,76 +3,130 @@ import '../styles/App.scss';
 import '../styles/core/reset.scss';
 import { useEffect, useState } from 'react';
 import callToApi from '../services/api';
+import logo from '../images/logofriends.png';
+
 function App() {
   const [dataPhrases, setDataPhrases] = useState([]);
-  const [newQuote, setNewQuote] = useState('');
-  const [newCharacter, setNewCharacter] = useState('');
+  const [newPhrase, setNewPhrase] = useState({
+    character: '',
+    quote: '',
+  });
+  const [filterQuote, setFilterQuote] = useState('');
+  const [filterCharacter, setFilterCharacter] = useState('Todos');
 
+  const handlefilterQuote = (ev) => {
+    setFilterQuote(ev.target.value);
+  };
+
+  const handleFilterCharacter = (ev) => {
+    setFilterCharacter(ev.target.value);
+  };
   useEffect(() => {
     callToApi().then((response) => {
       setDataPhrases(response);
     });
   }, []);
 
-  const hancleChangeQuote = (ev) => {
-    setNewQuote(ev.target.value);
-  };
-  const hancleChangeCharacter = (ev) => {
-    setNewCharacter(ev.target.value);
+  const hancleChangePhrase = (ev) => {
+    setNewPhrase({ ...newPhrase, [ev.target.id]: ev.target.value });
   };
 
-  const handleClickSavePhrase = () => {
-    const newPhrase = {
-      character: newCharacter,
-      quote: newQuote,
-    };
+  const handleClickSavePhrase = (ev) => {
+    ev.preventDefault();
     setDataPhrases([...dataPhrases, newPhrase]);
-    setNewCharacter('');
-    setNewQuote('');
+    setNewPhrase({
+      character: '',
+      quote: '',
+    });
   };
 
   const renderPhrases = () => {
-    return dataPhrases.map((phrase, index) => (
-      <li key={index} className="list__phrase">
-        <p>
-          {phrase.quote}{' '}
-          <span className="list__phrase-character"> - {phrase.character}</span>
-        </p>
-      </li>
-    ));
+    return dataPhrases
+      .filter((phrase) => {
+        return phrase.quote.toLowerCase().includes(filterQuote.toLowerCase());
+      })
+      .filter((phrase) => {
+        if (filterCharacter === 'Todos') {
+          return true;
+        }
+        return phrase.character === filterCharacter;
+      })
+      .map((phrase, index) => (
+        <li key={index} className="list__phrase">
+          <p className="list__phrase-quote">
+            {phrase.quote}{' '}
+            <span className="list__phrase-character">
+              {' '}
+              - {phrase.character}
+            </span>
+          </p>
+        </li>
+      ));
   };
 
   return (
     <div className="container">
       <header>
+        <img className="logo" alt="Logo Friends" href={logo} />
         <h1 className="title">Frases de Friends</h1>
       </header>
       <main className="main">
+        <form className="formFilter">
+          <label className="formFilter__label" htmlFor="quote">
+            Filtrar por frase
+          </label>
+          <input
+            className="formFilter__input"
+            type="text"
+            name="quote"
+            id="quote"
+            onChange={handlefilterQuote}
+          ></input>
+          <label className="form-filter__label" htmlFor="character">
+            Filtrar por personaje
+          </label>
+          <select
+            className="form-filter__select"
+            name="character"
+            id="character"
+            onChange={handleFilterCharacter}
+            value={filterCharacter}
+          >
+            <option value="Todos">Todos</option>
+            <option value="Chandler">Chandler</option>
+            <option value="Joey">Joey</option>
+            <option value="Monica">Monica</option>
+            <option value="Phoebe">Phoebe</option>
+            <option value="Rachel">Rachel</option>
+            <option value="Ross">Ross</option>
+          </select>
+        </form>
+
         <ul className="list">{renderPhrases()}</ul>
 
         <form className="form" action="">
           <h2 className="form__title">Añadir nueva frase</h2>
 
-          <label htmlFor="newQuote">Frase:</label>
+          <label htmlFor="quote">Frase:</label>
           <input
             placeholder=""
             type="text"
-            name="newQuote"
-            id="newQuote"
-            value={newQuote}
-            onChange={hancleChangeQuote}
+            name="quote"
+            id="quote"
+            value={newPhrase.quote}
+            onChange={hancleChangePhrase}
           />
 
-          <label htmlFor="newCharacter" className="form__label">
+          <label htmlFor="character" className="form__label">
             Personaje:
           </label>
           <input
             placeholder=""
             type="text"
-            name="newCharacter"
-            id="newCharacter"
-            value={newCharacter}
-            onChange={hancleChangeCharacter}
+            name="character"
+            id="character"
+            value={newPhrase.character}
+            onChange={hancleChangePhrase}
           />
           <input
             className="form__btn"
