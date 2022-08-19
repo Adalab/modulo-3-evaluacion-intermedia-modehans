71f7d754a3bfeaeffa11493abf5d0704268c1c58
modehans <monicaglezsanch@gmail.com>
2022-08-19 09:14:41 +0200

    Add initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// Fichero src/components/App.js
+import '../styles/App.scss';
+import '../styles/core/reset.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Frases de Friends</h1>
+    </div>
+  );
+}
+
+export default App;
